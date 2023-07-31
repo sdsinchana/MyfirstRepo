c9eb2172bb0c8ee084c3660ec69051253c825d31
Sinchana4 <sinchanasd241@gmail.com>
2023-07-31 18:07:24 +0530

    find the length od a string:

--- mer.py
+++ mer.py
@@ -5,6 +5,12 @@ for i in x:
     #print ASCII value
     for i in x:
         print(i,ord(i))
-
+#print upper case
 for i in x:
     print(i,i.upper(i))
+
+# to print length of string
+count=0
+for i in x:
+    count+=1
+print("the length is",count)
